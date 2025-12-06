de7629315d5e90a93dfe1a4a03c68d917795e39e
Hali2909 <moukrimehalima2909@gmail.com>
2025-12-06 13:10:19 +0000

    new file

--- scripts.js
+++ scripts.js
@@ -1,15 +1,17 @@
+require('dotenv').config();   // load variable 
+
+const endpoint = process.env.endpoint;  // legge la chiave
+
 console.log()
-
-const endpoint = 'post https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent?Key=AIzaSyDy2cX3iwczhR1bjVulXDIaKDoyubY34l8'
-
 const systemPrompt = 'You are Christ a friend that reply in a kinda and friendly way. Reply in english with a natural tone like a friend will do in a chat. keep answer short and spontaneous'
 //let's take the elements that we need 
 const input = document.querySelector('input')
 const button = document.querySelector('button')
 const ChatBox = document.querySelector('.chat-box') // in this case we don't have kust one div block so we take the class
-
-
+const contactStatus = document.querySelector('.contact-status')
 const messages = []
+
+//Enter/ arrow button
 button.addEventListener('click', SendMessage)
 input.addEventListener('keyup', function(event){
     if(event.key == 'Enter'){
@@ -28,6 +30,19 @@ function SendMessage(){
     getAnswerFromGemini()
 }
 
+
+function addMessage(MessageType, messageText){
+    const newMessage = {
+        type: MessageType,
+        text: messageText,
+        time: new Date().toLocaleString()
+    }
+    messages.push(newMessage)
+    ShowMessages()
+}
+
+
+
 function ShowMessages(){
     ChatBox.innerHTML = ''
 //for each message 
@@ -44,14 +59,21 @@ function ShowMessages(){
     }
 }
 
-function addMessage(MessageType, messageText){
-    const newMessage = {
-        type: MessageType,
-        text: messageText,
-        time: new Date().toLocaleString()
-    }
-    messages.push(newMessage)
-    ShowMessages()
+
+async function getAnswerFromGemini(){
+    const chatForGemini = formatChatForGemini()
+    //writing status
+    contactStatus.innerText = 'Is writing'
+    const response = await fetch(endpoint,{
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({contents: chatForGemini}) 
+    })
+
+    const data = await response.json()
+    const answer = data.candidates[0].content.parts[0].text
+    contactStatus.innerText = 'Online'
+    addMessage('received', answer)
 }
 
 
@@ -67,23 +89,13 @@ function formatChatForGemini(){
             role: message.type == 'sent' ? 'user' : 'model'
         })
     }
-    console.log(formattedChat)
-}
-
-formattedChat.unshift({
+    formattedChat.unshift({
     parts: [{text: systemPrompt }],
     role: 'user',
-})
-
-async function getAnswerFromGemini(){
-    const chatForGemini = formatChatForGemini()
-    const response = await fetch(endpoint,{
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({contents: chatForGemini}) 
-    
     })
-    const data = await response.json()
-    const answer = data.candidates[0].contents.parts[0].text
-    addMessage('receive', answer)
+    return formattedChat    
 }
+
+
+
+
